b08e5f727af310e3f73003b0f5790f4a09de89e9
sheldonu <uch20002@byui.edu>
2024-03-19 12:08:02 -0700

    lowest sugar added

--- fast_food.py
+++ fast_food.py
@@ -7,9 +7,11 @@ fast_food_data = pd.read_csv('FastFoodNutritionMenuV2.csv')
 # Display all the data in table like terms
 
 print(fast_food_data.describe)
+print()
+print()
 
 
-### Try to find out the food item that gives you the best ratio of calorie/protein. 
+### 1 Question: Try to find out the food item that gives you the best ratio of calorie/protein. 
 
 
 # Calculate protein-to-calorie ratio for each item
@@ -25,6 +27,8 @@ best_ratio_item = fast_food_data.loc[fast_food_data['Protein_to_Calorie_Ratio'].
 
 print("Item with the best protein-to-calorie ratio:")
 print(best_ratio_item[['Item', 'Calories', 'Protein (g)', 'Protein_to_Calorie_Ratio']])
+print()
+print()
 
 # This example above doesn't seem like it is right, I looked into the dataset where the Premium Hot Coffee is found, and I saw that
 # all the data from Taco Bell is one column behind for all the values, so it doesn't give the right value/answer.
@@ -47,3 +51,20 @@ best_ratio_item = filtered_data.loc[filtered_data['Protein_to_Calorie_Ratio'].id
 
 print("Item with the best protein-to-calorie ratio (excluding Taco Bell):")
 print(best_ratio_item[['Item', 'Calories', 'Protein (g)', 'Protein_to_Calorie_Ratio']])
+print()
+print()
+
+
+### 2 Question: Find the company with the most lowest amount of sugar items
+
+
+fast_food_data['Sugars (g)'] = pd.to_numeric(fast_food_data['Sugars (g)'], errors='coerce')
+
+# Group data by company and calculate the average sugar content for each company
+company_sugar_avg = fast_food_data.groupby('Company')['Sugars (g)'].mean().reset_index()
+
+# Find the company with the lowest average sugar content
+best_company = company_sugar_avg.loc[company_sugar_avg['Sugars (g)'].idxmin()]
+
+print("Company with the least average sugar content:")
+print(best_company[['Company', 'Sugars (g)']])
